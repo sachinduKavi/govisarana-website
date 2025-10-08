16da72ce5cc95cbaa58da0f87fea5371b35e0220
sachinduKavi <cst21017@std.uwu.ac.lk>
2025-10-08 18:04:13 +0530

    feat: toast message

--- model/System.ts
+++ model/System.ts
@@ -4,18 +4,14 @@ import { connecctionError, generalError } from '@/components/toast'
 import { checkPasscodeReqest } from '@/http/system-request'
 
 export async function checkPasscode(passcode: string) {
-  console.log('Checking passcode:', passcode)
   try {
     Loading.setLoading(true)
     const response = await checkPasscodeReqest(passcode)
-    console.log(response)
     if (response.status === 200 && response.data.proceed) {
       successToast('Passcode accepted. Redirecting...')
+    } else if (response.status !== 200) {
+      connecctionError()
     } else {
-      if (response.status !== 200) {
-        connecctionError()
-        return
-      }
       errorToast('Invalid passcode. Please try again.')
     }
   } catch (e: any) {
